c0ec3c5451c321bd695a82107f55d6160e99e59d
Julia Sivartsson <jsigc09@student.lnu.se>
2015-10-01 16:38:45 +0200

    Created Settings.php

--- src/model/dal/DatabaseConnection.php
+++ src/model/dal/DatabaseConnection.php
@@ -6,10 +6,10 @@ namespace model\dal;
 
 class DatabaseConnection
 {
-    private static $Dsn = 'localhost';
-    private static $Database = 'Workshop';
-    private static $User = 'AppUser';
-    private static $Password = 'PL6DvzxEqyxqFERK';
+    private static $Dsn = \Settings::DSN;
+    private static $Database = \Settings::DATABASE;
+    private static $User = \Settings::DB_USERNAME;
+    private static $Password = \Settings::DB_PASSWORD;
 
     public function Establish(){
         return new \PDO('mysql:host=' . self::$Dsn . ';dbname=' . self::$Database . ';', self::$User, self::$Password, array(\PDO::FETCH_OBJ));
