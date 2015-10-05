99e784b9d847e3b51748580bd7c4e2ebdf4b7972
ehamrin <eh222ve@student.lnu.se>
2015-10-05 09:45:55 +0200

    Refactor DAL class to extend DatabaseConnection

--- src/model/dal/DatabaseConnection.php
+++ src/model/dal/DatabaseConnection.php
@@ -4,14 +4,16 @@
 namespace model\dal;
 
 
-class DatabaseConnection
+abstract class DatabaseConnection
 {
-    private static $Dsn = \Settings::DSN;
-    private static $Database = \Settings::DATABASE;
-    private static $User = \Settings::DB_USERNAME;
-    private static $Password = \Settings::DB_PASSWORD;
+    public $db;
 
-    public function Establish(){
-        return new \PDO('mysql:host=' . self::$Dsn . ';dbname=' . self::$Database . ';', self::$User, self::$Password, array(\PDO::FETCH_OBJ));
+    public function __construct(){
+        try{
+            $this->db = new \PDO('mysql:host=' . \Settings::DSN . ';dbname=' . \Settings::DATABASE . ';', \Settings::DB_USERNAME, \Settings::DB_PASSWORD, array(\PDO::FETCH_OBJ));
+            $this->db->SetAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        }catch(\Exception $e){
+            throw $e;
+        }
     }
 }
